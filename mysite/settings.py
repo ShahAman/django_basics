919f2b2a3b46556faa27768ed51577635c68583e
ShahAman <shahnewaz@sgcsoft.net>
2022-01-10 15:10:15 +0600

    Added API CRUD

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def hello(request):
-    return HttpResponse("Hello World")
+    return render(request, 'hello.html', {"name": "ABC"})
