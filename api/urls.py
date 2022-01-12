0b4cf4dc7ff3e261c3e44c507709098ef63761a8
ShahAman <shahnewaz@sgcsoft.net>
2022-01-12 16:55:46 +0600

    Polls p_3 added

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.api_overview, name="api-overview"),
     path('task-list/', views.task_list, name="task-list"),
     path('task-create/', views.task_create, name='task-create'),
-    path('task-details/<str:pk>/', views.task_details, name='task-details'),
+    path('task-details/<int:pk>/', views.task_details, name='task-details'),
     path('task-update/<str:pk>/', views.task_update, name='task-update'),
     path('task-delete/<str:pk>/', views.task_delete, name='task-delete'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,15 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'playground',
     'api',
-    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
